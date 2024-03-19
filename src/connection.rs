e414b93027ec4445d24284fd48d16722b0c8261b
Robert <robert@pepzi.org>
2024-03-19 21:08:48 +0100

    wip

---
+++ src/connection.rs
@@ -0,0 +1,55 @@
+use bytes::BytesMut;
+use mini_redis::{Frame, Result};
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpStream;
+
+pub struct Connection {
+    stream: TcpStream,
+    buffer: BytesMut,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream,
+            // Allocate the buffer with 4kb of capacity.
+            buffer: BytesMut::with_capacity(4096),
+        }
+    }
+
+    /// Read a frame from the connection.
+    ///
+    /// Returns `None` if EOF is reached
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+        loop {
+            // Attempt to parse a frame from the buffered data. If
+            // enough data has been buffered, the frame is
+            // returned.
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame));
+            }
+
+            // There is not enough buffered data to read a frame.
+            // Attempt to read more data from the socket.
+            //
+            // On success, the number of bytes is returned. `0`
+            // indicated "end of stream".
+            if 0 == self.stream.read_buffer(&mut self.buffer).await? {
+                // The remote closed the connection. For this to be
+                // a clean shutdown, there should be no data in the
+                // read buffer. If there is, this means that the
+                // peer closed the socket while sending a frame.
+                if self.buffer.is_empty() {
+                    return Ok(None);
+                } else {
+                    return Err("connection reset by peer".into());
+                }
+            }
+        }
+    }
+
+    /// Write a frame to the connection.
+    pub async fn write_frame(&mut self, frame: &Frame) -> Result<()> {
+        todo!();
+    }
+}
